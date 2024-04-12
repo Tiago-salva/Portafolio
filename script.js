ba846ea63d4f75e08bda442cc9a5ea9f6dfd4936
Tiago-salva <maitetiago58@gmail.com>
2024-04-12 19:06:10 -0300

    Added a simple verification for the contact form

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const headerName = document.querySelector(".header__name");
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const headerNav = document.querySelector(".header__nav");
 const allHeaderNavItem = document.querySelectorAll(".header__nav-item");
+const contactoForm = document.querySelector(".contacto__form");
 const allContactoFormInput = document.querySelectorAll(".contacto__form-input");
+const allErrorMessage = document.querySelectorAll(".error-message");
+const btnEnviarMensaje = document.querySelector(".btn-enviar-mensaje");
 const line1 = document.querySelector(".line-1");
 const line2 = document.querySelector(".line-2");
 const line3 = document.querySelector(".line-3");
 
 window.addEventListener("DOMContentLoaded", () => {
-  console.log(screen.width);
   if (screen.width >= 768) {
     headerName.textContent = "Tiago Salvatierra";
   }
@@ -32,8 +34,13 @@ allHeaderNavItem.forEach((navItem) => {
   });
 });
 
-allContactoFormInput.forEach((input) => {
+allContactoFormInput.forEach((input, index) => {
   input.addEventListener("input", () => {
+    allErrorMessage[index].textContent = "";
+    allErrorMessage[index].classList.remove("displayed");
+
+    showError(input, index);
+
     if (input.value !== "") {
       input.classList.add("no-empty");
     } else {
@@ -41,3 +48,46 @@ allContactoFormInput.forEach((input) => {
     }
   });
 });
+
+contactoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (!checkErrors()) {
+    allContactoFormInput.forEach((input) => {
+      input.value = "";
+      input.classList.remove("no-empty");
+    });
+
+    btnEnviarMensaje.classList.add("sent");
+    setTimeout(() => {
+      btnEnviarMensaje.classList.remove("sent");
+    }, 1500);
+  }
+});
+
+function showError(input, index) {
+  if (input.validity.valueMissing) {
+    allErrorMessage[index].classList.add("displayed");
+    allErrorMessage[index].textContent = "El input no puede estar vacio";
+    return true;
+  } else if (input.validity.typeMismatch) {
+    allErrorMessage[index].classList.add("displayed");
+    allErrorMessage[index].textContent = "El input debe ser un E-mail valido";
+    return true;
+  } else if (input.validity.tooShort) {
+    allErrorMessage[index].classList.add("displayed");
+    allErrorMessage[index].textContent =
+      "El input debe tener al menos 50 caracteres";
+    return true;
+  }
+}
+
+function checkErrors() {
+  let thereIsSomeError = false;
+
+  allContactoFormInput.forEach((input, index) => {
+    thereIsSomeError = showError(input, index);
+  });
+
+  return thereIsSomeError;
+}
