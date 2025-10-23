f45c8c05cc621dd351487baff76b77f9a68c130c
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-23 09:38:14 -0300

    feat: create a simple hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,35 @@
+import Nav from "./Nav";
+
+export default function Hero() {
+  return (
+    <section>
+      <h1>
+        Hello, I'm <br /> Tiago
+      </h1>
+
+      <Nav />
+
+      <div>
+        <p>
+          Text about me: Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Porro veniam reiciendis dolor quam aliquid tenetur cumque odio
+          provident, eos molestiae facere officia ea et rerum expedita inventore
+          natus laudantium velit? Tempore, inventore reiciendis harum officia
+          repellat ad! Tenetur esse, illum blanditiis iure totam aliquam facilis
+          repudiandae eius pariatur nesciunt corrupti quod delectus? Placeat
+          laboriosam ut earum temporibus odit ducimus accusamus! Eveniet cumque
+          vero cupiditate. Veniam perspiciatis, exercitationem magnam libero
+          modi assumenda accusantium. Velit quibusdam totam architecto ipsum
+          voluptatem veritatis natus, repellat quaerat deleniti nobis modi
+          blanditiis sed debitis. Ea, voluptas. Minus quibusdam alias illo
+          nulla. Repellat explicabo in fuga sapiente autem quis est magnam.
+          Dolores corrupti blanditiis laborum odio consectetur dolore dolorem
+          deserunt aut, ea nesciunt perferendis tempore vitae ducimus! Incidunt
+          commodi eius nemo quasi doloremque cupiditate! Voluptates nemo atque
+          nostrum ipsum temporibus, animi ut perferendis sunt ullam sint optio
+          ipsa? Neque consequuntur totam at molestias laborum nihil id odit.
+        </p>
+      </div>
+    </section>
+  );
+}
