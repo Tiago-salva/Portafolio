e69a2884ac49621dd756740ff4ce173ce2c387ff
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-23 09:38:35 -0300

    feat: create a simple Nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+export default function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <button className="cursor-pointer">Navigation</button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
