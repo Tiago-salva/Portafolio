84f0df117991d4bf26df4247e33bb13944a8aff7
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-27 19:10:25 -0300

    feat: update component styles

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,7 +2,7 @@ export default function Section({ id, children }) {
   return (
     <section
       id={id}
-      className="min-h-dvh w-1/2 ml-auto flex flex-col justify-center items-center p-20 text-center"
+      className="min-h-dvh flex flex-col justify-center items-center p-20 text-center"
     >
       {children}
     </section>
