70b9c3c1a150b82f2509dcae1f89895f132f5596
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-27 10:55:03 -0300

    feat: add navigation links and styling classes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,19 @@
 export default function Nav() {
   return (
     <nav>
-      <ul>
-        <li>
-          <button className="cursor-pointer">Navigation</button>
-        </li>
+      <ul className="flex flex-col">
+        <a href="#about-me" className="flex items-center group">
+          <div className="w-8 h-0.5 bg-black mr-5 group-hover:w-12 transition-all"></div>
+          <p>About me</p>
+        </a>
+        <a href="#skills" className="flex items-center group">
+          <div className="w-8 h-0.5 bg-black mr-5 group-hover:w-12 transition-all"></div>
+          <p>Skills</p>
+        </a>
+        <a href="#projects" className="flex items-center group">
+          <div className="w-8 h-0.5 bg-black mr-5 group-hover:w-12 transition-all"></div>
+          <p>Projects</p>
+        </a>
       </ul>
     </nav>
   );
