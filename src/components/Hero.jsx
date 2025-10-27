d210e06257c6c79924da96e5b2523a8cdbccf7eb
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-27 19:09:45 -0300

    feat: update Hero component layout and styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,20 @@
+import Footer from "./Footer";
 import Nav from "./Nav";
 
 export default function Hero() {
   return (
-    <section className="flex justify-between min-h-dvh p-20 fixed">
-      <div className="min-w-2/5 flex flex-col justify-center">
-        <h1 className="text-5xl bottom-8 relative">
+    <section className="flex flex-col justify-center min-h-dvh p-10 fixed bg-amber-50 min-w-2/6">
+      <div className="min-w-2/5 flex flex-col justify-center m-auto">
+        <h1 className="text-6xl font-bold relative">
           Hello, I'm <br /> Tiago
         </h1>
 
+        <p className="mt-4 mb-8">Frontend Developer</p>
+
         <Nav />
       </div>
+
+      <Footer />
     </section>
   );
 }
