8010d40289a3bbe663f8102666851195e4fafeb7
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-27 10:54:36 -0300

    feat: refactor Hero component layout and styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,33 +2,13 @@ import Nav from "./Nav";
 
 export default function Hero() {
   return (
-    <section>
-      <h1>
-        Hello, I'm <br /> Tiago
-      </h1>
+    <section className="flex justify-between min-h-dvh p-20 fixed">
+      <div className="min-w-2/5 flex flex-col justify-center">
+        <h1 className="text-5xl bottom-8 relative">
+          Hello, I'm <br /> Tiago
+        </h1>
 
-      <Nav />
-
-      <div>
-        <p>
-          Text about me: Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Porro veniam reiciendis dolor quam aliquid tenetur cumque odio
-          provident, eos molestiae facere officia ea et rerum expedita inventore
-          natus laudantium velit? Tempore, inventore reiciendis harum officia
-          repellat ad! Tenetur esse, illum blanditiis iure totam aliquam facilis
-          repudiandae eius pariatur nesciunt corrupti quod delectus? Placeat
-          laboriosam ut earum temporibus odit ducimus accusamus! Eveniet cumque
-          vero cupiditate. Veniam perspiciatis, exercitationem magnam libero
-          modi assumenda accusantium. Velit quibusdam totam architecto ipsum
-          voluptatem veritatis natus, repellat quaerat deleniti nobis modi
-          blanditiis sed debitis. Ea, voluptas. Minus quibusdam alias illo
-          nulla. Repellat explicabo in fuga sapiente autem quis est magnam.
-          Dolores corrupti blanditiis laborum odio consectetur dolore dolorem
-          deserunt aut, ea nesciunt perferendis tempore vitae ducimus! Incidunt
-          commodi eius nemo quasi doloremque cupiditate! Voluptates nemo atque
-          nostrum ipsum temporibus, animi ut perferendis sunt ullam sint optio
-          ipsa? Neque consequuntur totam at molestias laborum nihil id odit.
-        </p>
+        <Nav />
       </div>
     </section>
   );
