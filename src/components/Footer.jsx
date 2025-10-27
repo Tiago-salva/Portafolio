0a7469d176125deabe96b9f1beddfe2c507a6d3e
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-27 10:53:53 -0300

    feat: add Footer component with social links

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="h-20 bg-amber-100 flex justify-center">
+      <ul className="flex justify-center items-center gap-3">
+        <li>Github</li>
+        <li>Linkedin</li>
+      </ul>
+    </footer>
+  );
+}
