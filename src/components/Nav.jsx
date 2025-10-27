5811a9a02da19b15b99a63dcaa9b571f747204a3
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-27 19:10:04 -0300

    feat: update Nav component text styling for better visibility

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,15 +4,15 @@ export default function Nav() {
       <ul className="flex flex-col">
         <a href="#about-me" className="flex items-center group">
           <div className="w-8 h-0.5 bg-black mr-5 group-hover:w-12 transition-all"></div>
-          <p>About me</p>
+          <p className="text-2xl">About me</p>
         </a>
         <a href="#skills" className="flex items-center group">
           <div className="w-8 h-0.5 bg-black mr-5 group-hover:w-12 transition-all"></div>
-          <p>Skills</p>
+          <p className="text-2xl">Skills</p>
         </a>
         <a href="#projects" className="flex items-center group">
           <div className="w-8 h-0.5 bg-black mr-5 group-hover:w-12 transition-all"></div>
-          <p>Projects</p>
+          <p className="text-2xl">Projects</p>
         </a>
       </ul>
     </nav>
