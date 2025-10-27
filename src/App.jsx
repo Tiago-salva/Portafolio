c2746f9dc5b7a99ad6e592a7e7c0d9357c697e17
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-27 19:08:32 -0300

    feat: add SVG icon for dark mode toggle and adjust layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import { useState } from "react";
 import Hero from "./components/Hero";
 import Section from "./components/Section";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <button className="right-5 top-5 cursor-pointer fixed">Dark mode</button>
+      <button className="right-5 top-5 cursor-pointer fixed">
+        <svg
+          className="w-10 h-10 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          fill="balck"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 21a9 9 0 0 1-.5-17.986V3c-.354.966-.5 1.911-.5 3a9 9 0 0 0 9 9c.239 0 .254.018.488 0A9.004 9.004 0 0 1 12 21Z"
+          />
+        </svg>
+      </button>
+
       <Hero />
 
-      <main>
+      <main className="w-2/3 flex flex-col justify-center items-center ml-auto">
         <Section>
           <div className="flex justify-center items-center">
             <p>Algo</p>
@@ -45,8 +63,6 @@ function App() {
         <Section id={"projects"}>
           <p>Projects</p>
         </Section>
-
-        <Footer />
       </main>
     </>
   );
