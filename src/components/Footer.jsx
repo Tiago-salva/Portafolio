69908e12bb5c1344de93f332debbb11aad5b850d
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-27 19:09:28 -0300

    feat: include GitHub and LinkedIn icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,25 @@
 export default function Footer() {
   return (
-    <footer className="h-20 bg-amber-100 flex justify-center">
+    <footer className="h-20 w-full flex justify-center items-center">
       <ul className="flex justify-center items-center gap-3">
-        <li>Github</li>
-        <li>Linkedin</li>
+        <li className="group">
+          <a href="#">
+            <img
+              src="../../public/github-brands-solid-full.svg"
+              className="w-10 h-10 group-hover:bg-amber-200 transition-all"
+              alt="github"
+            />
+          </a>
+        </li>
+        <li className="group">
+          <a href="#">
+            <img
+              src="../../public/linkedin-brands-solid-full.svg"
+              className="w-10 h-10 group-hover:bg-amber-200 transition-all"
+              alt="linkedin"
+            />
+          </a>
+        </li>
       </ul>
     </footer>
   );
