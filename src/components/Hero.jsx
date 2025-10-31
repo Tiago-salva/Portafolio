b544b5d3927f768e796438b8331cad006392e9f1
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-31 09:00:06 -0300

    feat: add profession animation and improved layout design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,21 @@ import Nav from "./Nav";
 
 export default function Hero() {
   return (
-    <section className="flex flex-col justify-center min-h-dvh p-10 fixed bg-amber-50 min-w-2/6">
+    <section className="flex flex-col justify-center min-h-dvh p-10 fixed bg-white min-w-2/6 border-r-4 animate-leftSlide">
       <div className="min-w-2/5 flex flex-col justify-center m-auto">
-        <h1 className="text-6xl font-bold relative">
-          Hello, I'm <br /> Tiago
-        </h1>
+        <h1 className="text-6xl font-bold relative">Hello, I'm</h1>
+        <h1 className="text-6xl font-bold relative text-blue-300">Tiago</h1>
 
-        <p className="mt-4 mb-8">Frontend Developer</p>
+        <div className="h-5 overflow-hidden mt-4 mb-8 ">
+          <div
+            id="profession-container"
+            className="transition-transform animate-moveUp"
+          >
+            <p>Frontend Developer</p>
+            <p>Backend Developer</p>
+            <p>Student</p>
+          </div>
+        </div>
 
         <Nav />
       </div>
