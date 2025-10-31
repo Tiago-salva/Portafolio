9bb7fc2188b1613694d6248f44d6a0098c0ddaa5
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-31 08:54:37 -0300

    feat: add hover animations for buttons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,20 +2,20 @@ export default function Footer() {
   return (
     <footer className="h-20 w-full flex justify-center items-center">
       <ul className="flex justify-center items-center gap-3">
-        <li className="group">
-          <a href="#">
+        <li className="group w-12">
+          <a href="https://github.com/Tiago-salva">
             <img
               src="../../public/github-brands-solid-full.svg"
-              className="w-10 h-10 group-hover:bg-amber-200 transition-all"
+              className="w-full h-full group-hover:scale-120 transition-all"
               alt="github"
             />
           </a>
         </li>
-        <li className="group">
-          <a href="#">
+        <li className="group w-12">
+          <a href="https://www.linkedin.com/in/tiago-salvatierra-71a70a384/">
             <img
               src="../../public/linkedin-brands-solid-full.svg"
-              className="w-10 h-10 group-hover:bg-amber-200 transition-all"
+              className="w-full h-full group-hover:scale-120 transition-all"
               alt="linkedin"
             />
           </a>
