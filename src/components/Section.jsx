6402dbb0337a2c5de38535200e7dcdf98db17052
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-31 09:01:29 -0300

    feat: improve layout styles

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,7 +2,7 @@ export default function Section({ id, children }) {
   return (
     <section
       id={id}
-      className="min-h-dvh flex flex-col justify-center items-center p-20 text-center"
+      className="min-h-dvh w-full flex flex-col justify-center items-center text-center"
     >
       {children}
     </section>
