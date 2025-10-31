4f43ddc873bed6fe63079bcea010897a25d6113c
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-10-31 09:01:48 -0300

    feat: add button's hover animations

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,15 +3,15 @@ export default function Nav() {
     <nav>
       <ul className="flex flex-col">
         <a href="#about-me" className="flex items-center group">
-          <div className="w-8 h-0.5 bg-black mr-5 group-hover:w-12 transition-all"></div>
+          <div className="w-8 h-0.5 bg-black mr-5 group-hover:w-12 group-hover:bg-blue-300 transition-all"></div>
           <p className="text-2xl">About me</p>
         </a>
         <a href="#skills" className="flex items-center group">
-          <div className="w-8 h-0.5 bg-black mr-5 group-hover:w-12 transition-all"></div>
+          <div className="w-8 h-0.5 bg-black mr-5 group-hover:w-12 group-hover:bg-blue-300 transition-all"></div>
           <p className="text-2xl">Skills</p>
         </a>
         <a href="#projects" className="flex items-center group">
-          <div className="w-8 h-0.5 bg-black mr-5 group-hover:w-12 transition-all"></div>
+          <div className="w-8 h-0.5 bg-black mr-5 group-hover:w-12 group-hover:bg-blue-300 transition-all"></div>
           <p className="text-2xl">Projects</p>
         </a>
       </ul>
