f33bdc595ca0356b23863a52a3f0b726a023333b
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-11-01 10:32:55 -0300

    feat: implement background and customizable color

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,8 +1,15 @@
-export default function Section({ id, children }) {
+export default function Section({
+  id,
+  children,
+  withBackground,
+  bgColor = "bg-blue-300",
+}) {
   return (
     <section
       id={id}
-      className="min-h-dvh w-full flex flex-col justify-center items-center text-center"
+      className={`min-h-dvh w-full flex flex-col justify-center items-center text-center p-20 ${
+        withBackground ? bgColor : ""
+      } z-10 shadow-xl`}
     >
       {children}
     </section>
