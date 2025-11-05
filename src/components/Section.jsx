15959236e5c8fd576ccf33188d7335ec4763287e
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-11-05 18:10:04 -0300

    feat: add specificStyles prop to the Section component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,14 +2,15 @@ export default function Section({
   id,
   children,
   withBackground,
-  bgColor = "bg-blue-300",
+  bgColor = "bg-gray-100",
+  specificStyles,
 }) {
   return (
     <section
       id={id}
-      className={`min-h-dvh w-full flex flex-col justify-center items-center text-center p-20 ${
+      className={`${specificStyles}  min-h-dvh w-full flex flex-col justify-center items-center text-center p-20 g-10 ${
         withBackground ? bgColor : ""
-      } z-10 shadow-xl`}
+      } z-10`}
     >
       {children}
     </section>
