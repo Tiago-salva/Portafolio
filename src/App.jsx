37f55a656c554611ffc239bc2f9aaf4cd1bdb0ee
Tiago Salvatierra <tiagosalvatierra258@gmail.com>
2025-11-05 18:03:04 -0300

    feat: enhance About Me section and add WelcomeTitle component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
 import Hero from "./components/Hero";
 import Section from "./components/Section";
 import Carousel from "./components/Carousel";
+import Welcome from "./components/WelcomeTitle";
 
 function App() {
   return (
@@ -29,29 +29,40 @@ function App() {
       <Hero />
 
       <main className="w-2/3 flex flex-col justify-center items-center ml-auto">
-        <Section>
-          <div className="flex justify-center items-center w-full overflow-hidden">
+        <Section
+          withBackground
+          bgColor="bg-gradient-to-b from-gray-100/0 to-blue-200/80"
+          id={"carousel"}
+        >
+          <div className="flex justify-center items-center b-ra w-full overflow-hidden">
+            <Welcome />
             <Carousel />
           </div>
         </Section>
 
         <Section withBackground id={"about-me"}>
-          <h2 className="text-4xl font-bold mb-5">About me</h2>
-          <p className="text-2xl text-center w-4/6 leading-relaxed mb-5">
-            I'm a frontend developer who enjoys creating websites to solve
-            problems, I make sure my sites are not only clear, structured, and
-            clean but also fully functional.
-          </p>
-          <p className="text-2xl text-center w-4/6 leading-relaxed mb-5">
-            I love learning how things work under the hood and I'm always
-            looking for something new to learn, from programming languages to
-            development tools. I'm currently building new projects to share in
-            this portfolio and learning technologies like Typescript and Docker
-            to stay up to date.
-          </p>
-          <p className="text-2xl text-center w-4/6 leading-relaxed">
-            In my spare time, I play videogames or watching a movie or a series
-          </p>
+          <h2 className="text-6xl font-bold mb-20 text-blue-300">About me</h2>
+          <div className="flex flex-col justify-center items-center gap-5">
+            <p className="text-2xl text-center w-4/6 leading-relaxed mb-5">
+              I'm a <strong>fullstack developer</strong> who enjoys building
+              projects that go beyond simple examples like landing pages or
+              small apps. I like creating things with a real purpose, projects
+              that aim to solve a problem or make something easier, not just
+              look good.
+            </p>
+            <p className="text-2xl text-center w-4/6 leading-relaxed mb-5">
+              I love learning how things work under the hood and I'm always
+              looking for something new to learn, from programming languages to
+              development tools. I'm currently building new projects to share in
+              this portfolio and learning technologies like
+              <strong> Typescript</strong> and <strong>Docker</strong> to stay
+              up to date.
+            </p>
+            <p className="text-2xl text-center w-4/6 leading-relaxed">
+              When I'm not coding, I love playing videogames, watching movies,
+              or diving into something new to learn.
+            </p>
+          </div>
         </Section>
 
         <Section withBackground bgColor="bg-gray-200" id={"skills"}>
